7b06cb3c28ab5bd5e9e437479fad570e27a97fc7
Andrey Alferov <andytower@mail.ru>
2018-11-18 16:56:18 +0300

    Read EXIFTag from jpeg. Use Pillow

--- resortPhotoByDate.py
+++ resortPhotoByDate.py
@@ -1,5 +1,17 @@
 import os
 import mimetypes
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+
+def get_exif(fn):
+    ret = {}
+    i = Image.open(fn)
+    info = i._getexif()
+    for tag, value in info.items():
+        decoded = TAGS.get(tag, tag)
+        ret[decoded] = value
+    return ret
 
 
 def findAllFileInDirectory(pDir):
@@ -8,7 +20,7 @@ def findAllFileInDirectory(pDir):
         for vCurrentFile in vFiles:
             print(f'Current file - {vCurrentFile}')
             if mimetypes.guess_type(f'{os.path.join(vCurDir,vCurrentFile)}')[0].split('/')[1] == 'jpeg':
-                print('This is jpeg')
+                print(get_exif(os.path.join(vCurDir, vCurrentFile)))
         for vCurrentSubDir in vSubDirs:
             findAllFileInDirectory(vCurrentSubDir)
 
